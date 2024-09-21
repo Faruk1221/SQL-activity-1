755eb85a5368636506f1e20b5fd0dcf3f648248f
Faruk1221 <mdomorfaruk00100@gmail.com>
2024-09-21 23:35:24 +0600

    Add files via upload

---
+++ activity2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE supplier(
+SNO TEXT, 
+SNAME TEXT,
+STATUS INT,
+CITY TEXT
+);
+SELECT * FROM supplier;
+INSERT INTO supplier (SNO ,SNAME,STATUS,CITY) VALUES
+("S1","smits",20,"london"),
+("s2","jon's",10,"paris"),
+("s3","blake",30,"paris"),
+("s4", "clarke",20,"london"),
+("s5","adamas",30,"atsens");
